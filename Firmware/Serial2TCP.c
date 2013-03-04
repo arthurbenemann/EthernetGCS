b2cb2dd2b13b0fbfacf8a2b3fe792fed193f6dc7
Arthur Benemann <arthur.benemann@gmail.com>
2013-03-04 11:58:40 -0300

    Refactoring the Serial2TCP file.

--- Firmware/Serial2TCP.c
+++ Firmware/Serial2TCP.c
@@ -1,11 +1,7 @@
 #include "TCPIPConfig.h"
 #include "HardwareProfile.h"
-
-#define Serial2TCP_PORT	9761
-#define BAUD_RATE		57600ul
-
-
 #include "TCPIP Stack/TCPIP.h"
+#include "Serial2TCP.h"
 
 // Ring buffers for transfering data to and from the UART ISR:
 //  - (Head pointer == Tail pointer) is defined as an empty FIFO
@@ -16,45 +12,22 @@ static BYTE vUARTRXFIFO[65];
 static BYTE vUARTTXFIFO[17];
 static volatile BYTE *RXHeadPtr = vUARTRXFIFO, *RXTailPtr = vUARTRXFIFO;
 static volatile BYTE *TXHeadPtr = vUARTTXFIFO, *TXTailPtr = vUARTTXFIFO;
+static BYTE *RXHeadPtrShadow, *RXTailPtrShadow;
+static BYTE *TXHeadPtrShadow, *TXTailPtrShadow;
 
-/**
- * Sets up the UART peripheral for this application
- */
-void Serial2TCPInit(void) {
-    UMODE = 0x8000; // Set UARTEN.  Note: this must be done before setting UTXEN
+static enum _BridgeState {
+    SM_HOME = 0,
+    SM_SOCKET_OBTAINED
+} BridgeState = SM_HOME;
+static TCP_SOCKET MySocket = INVALID_SOCKET;
 
-    USTA = 0x0400; // UTXEN set
-#define CLOSEST_UBRG_VALUE ((GetPeripheralClock()+8ul*BAUD_RATE)/16/BAUD_RATE-1)
-#define BAUD_ACTUAL (GetPeripheralClock()/16/(CLOSEST_UBRG_VALUE+1))
-
-
-#define BAUD_ERROR ((BAUD_ACTUAL > BAUD_RATE) ? BAUD_ACTUAL-BAUD_RATE : BAUD_RATE-BAUD_ACTUAL)
-#define BAUD_ERROR_PRECENT	((BAUD_ERROR*100+BAUD_RATE/2)/BAUD_RATE)
-#if (BAUD_ERROR_PRECENT > 3)
-#warning UART frequency error is worse than 3%
-#elif (BAUD_ERROR_PRECENT > 2)
-#warning UART frequency error is worse than 2%
-#endif
-
-    UBRG = CLOSEST_UBRG_VALUE;
-
-}
+void receiveTcpDataToTxFifo(TCP_SOCKET MySocket);
+void sendRxDataToTcp(TCP_SOCKET MySocket);
 
 /**
  * Exchange data between TCP and the two Serial fifo buffers
  */
 void Serial2TCPTask(void) {
-
-    static enum _BridgeState {
-        SM_HOME = 0,
-        SM_SOCKET_OBTAINED
-    } BridgeState = SM_HOME;
-    static TCP_SOCKET MySocket = INVALID_SOCKET;
-    WORD wMaxPut, wMaxGet, w;
-    BYTE *RXHeadPtrShadow, *RXTailPtrShadow;
-    BYTE *TXHeadPtrShadow, *TXTailPtrShadow;
-
-
     switch (BridgeState) {
         case SM_HOME:
             MySocket = TCPOpen(0, TCP_OPEN_SERVER, Serial2TCP_PORT, TCP_PURPOSE_UART_2_TCP_BRIDGE);
@@ -111,62 +84,11 @@ void Serial2TCPTask(void) {
             if (TXHeadPtrShadow != TXTailPtrShadow)
                 IEC1bits.U2TXIE = 1;
 
-            //
-            // Transmit pending data that has been placed into the UART RX FIFO (in the ISR)
-            //
-            wMaxPut = TCPIsPutReady(MySocket); // Get TCP TX FIFO space
-            wMaxGet = RXHeadPtrShadow - RXTailPtrShadow; // Get UART RX FIFO byte count
-            if (RXHeadPtrShadow < RXTailPtrShadow)
-                wMaxGet += sizeof (vUARTRXFIFO);
-            if (wMaxPut > wMaxGet) // Calculate the lesser of the two
-                wMaxPut = wMaxGet;
-            if (wMaxPut) // See if we can transfer anything
-            {
-                // Transfer the data over.  Note that a two part put
-                // may be needed if the data spans the vUARTRXFIFO
-                // end to start address.
-                w = vUARTRXFIFO + sizeof (vUARTRXFIFO) - RXTailPtrShadow;
-                if (wMaxPut >= w) {
-                    TCPPutArray(MySocket, RXTailPtrShadow, w);
-                    RXTailPtrShadow = vUARTRXFIFO;
-                    wMaxPut -= w;
-                }
-                TCPPutArray(MySocket, RXTailPtrShadow, wMaxPut);
-                RXTailPtrShadow += wMaxPut;
 
-                // No flush.  The stack will automatically flush and do
-                // transmit coallescing to minimize the number of TCP
-                // packets that get sent.  If you explicitly call TCPFlush()
-                // here, latency will go down, but so will max throughput
-                // and bandwidth efficiency.
-            }
-
-            //
-            // Transfer received TCP data into the UART TX FIFO for future transmission (in the ISR)
-            //
-            wMaxGet = TCPIsGetReady(MySocket); // Get TCP RX FIFO byte count
-            wMaxPut = TXTailPtrShadow - TXHeadPtrShadow - 1; // Get UART TX FIFO free space
-            if (TXHeadPtrShadow >= TXTailPtrShadow)
-                wMaxPut += sizeof (vUARTTXFIFO);
-            if (wMaxPut > wMaxGet) // Calculate the lesser of the two
-                wMaxPut = wMaxGet;
-            if (wMaxPut) // See if we can transfer anything
-            {
-                // Transfer the data over.  Note that a two part put
-                // may be needed if the data spans the vUARTTXFIFO
-                // end to start address.
-                w = vUARTTXFIFO + sizeof (vUARTTXFIFO) - TXHeadPtrShadow;
-                if (wMaxPut >= w) {
-                    TCPGetArray(MySocket, TXHeadPtrShadow, w);
-                    TXHeadPtrShadow = vUARTTXFIFO;
-                    wMaxPut -= w;
-                }
-                TCPGetArray(MySocket, TXHeadPtrShadow, wMaxPut);
-                TXHeadPtrShadow += wMaxPut;
-            }
+            receiveTcpDataToTxFifo(MySocket);
+            sendRxDataToTcp(MySocket);
 
             // Write local shadowed FIFO pointers into the volatile FIFO pointers.
-
             IEC1bits.U2RXIE = 0;
             IEC1bits.U2TXIE = 0;
 
@@ -178,6 +100,65 @@ void Serial2TCPTask(void) {
                 IEC1bits.U2TXIE = 1;
 
             break;
+    }
+}
+
+/**
+ * // Transfer received TCP data into the UART TX FIFO for future transmission (in the ISR)
+ */
+void receiveTcpDataToTxFifo(TCP_SOCKET MySocket) {
+    WORD wMaxPut, wMaxGet, w;
+    wMaxGet = TCPIsGetReady(MySocket); // Get TCP RX FIFO byte count
+    wMaxPut = TXTailPtrShadow - TXHeadPtrShadow - 1; // Get UART TX FIFO free space
+    if (TXHeadPtrShadow >= TXTailPtrShadow)
+        wMaxPut += sizeof (vUARTTXFIFO);
+    if (wMaxPut > wMaxGet) // Calculate the lesser of the two
+        wMaxPut = wMaxGet;
+    if (wMaxPut) // See if we can transfer anything
+    {
+        // Transfer the data over.  Note that a two part put
+        // may be needed if the data spans the vUARTTXFIFO
+        // end to start address.
+        w = vUARTTXFIFO + sizeof (vUARTTXFIFO) - TXHeadPtrShadow;
+        if (wMaxPut >= w) {
+            TCPGetArray(MySocket, TXHeadPtrShadow, w);
+            TXHeadPtrShadow = vUARTTXFIFO;
+            wMaxPut -= w;
+        }
+        TCPGetArray(MySocket, TXHeadPtrShadow, wMaxPut);
+        TXHeadPtrShadow += wMaxPut;
+    }
+}
+/*
+ *  Transmit pending data that has been placed into the UART RX FIFO (in the ISR)
+ */
+void sendRxDataToTcp(TCP_SOCKET MySocket) {
+    WORD wMaxPut, wMaxGet, w;
+    wMaxPut = TCPIsPutReady(MySocket); // Get TCP TX FIFO space
+    wMaxGet = RXHeadPtrShadow - RXTailPtrShadow; // Get UART RX FIFO byte count
+    if (RXHeadPtrShadow < RXTailPtrShadow)
+        wMaxGet += sizeof (vUARTRXFIFO);
+    if (wMaxPut > wMaxGet) // Calculate the lesser of the two
+        wMaxPut = wMaxGet;
+    if (wMaxPut) // See if we can transfer anything
+    {
+        // Transfer the data over.  Note that a two part put
+        // may be needed if the data spans the vUARTRXFIFO
+        // end to start address.
+        w = vUARTRXFIFO + sizeof (vUARTRXFIFO) - RXTailPtrShadow;
+        if (wMaxPut >= w) {
+            TCPPutArray(MySocket, RXTailPtrShadow, w);
+            RXTailPtrShadow = vUARTRXFIFO;
+            wMaxPut -= w;
+        }
+        TCPPutArray(MySocket, RXTailPtrShadow, wMaxPut);
+        RXTailPtrShadow += wMaxPut;
+
+        // No flush.  The stack will automatically flush and do
+        // transmit coallescing to minimize the number of TCP
+        // packets that get sent.  If you explicitly call TCPFlush()
+        // here, latency will go down, but so will max throughput
+        // and bandwidth efficiency.
     }
 }
 
@@ -222,6 +203,27 @@ void _ISR __attribute__((__no_auto_psv__)) _U2TXInterrupt(void) {
     }
 }
 
+/**
+ * Sets up the UART peripheral for this application
+ */
+void Serial2TCPInit(void) {
+    UMODE = 0x8000; // Set UARTEN.  Note: this must be done before setting UTXEN
 
+    USTA = 0x0400; // UTXEN set
+#define CLOSEST_UBRG_VALUE ((GetPeripheralClock()+8ul*BAUD_RATE)/16/BAUD_RATE-1)
+#define BAUD_ACTUAL (GetPeripheralClock()/16/(CLOSEST_UBRG_VALUE+1))
+
+
+#define BAUD_ERROR ((BAUD_ACTUAL > BAUD_RATE) ? BAUD_ACTUAL-BAUD_RATE : BAUD_RATE-BAUD_ACTUAL)
+#define BAUD_ERROR_PRECENT	((BAUD_ERROR*100+BAUD_RATE/2)/BAUD_RATE)
+#if (BAUD_ERROR_PRECENT > 3)
+#warning UART frequency error is worse than 3%
+#elif (BAUD_ERROR_PRECENT > 2)
+#warning UART frequency error is worse than 2%
+#endif
+
+    UBRG = CLOSEST_UBRG_VALUE;
+
+}
 
 
--- Firmware/Serial2TCP.h
+++ Firmware/Serial2TCP.h
@@ -1,3 +1,6 @@
+#define Serial2TCP_PORT	9761
+#define BAUD_RATE	57600ul
+
 void Serial2TCPInit(void);
 void Serial2TCPTask(void);
 
